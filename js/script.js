59570fb518723cac31c281b7a726944fa8317394
ulisse-fontanot <ulissef99@gmail.com>
2021-01-25 15:33:06 +0100

    init-fine +bonus

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var root = new Vue({
+  el: '#root',
+  data: {
+    message: 'BUONASERAAA',
+    src: 'https://i.ytimg.com/vi/-cMAOHXcwGs/hqdefault.jpg',
+    titolo: 'buonasera',
+    mieClassi: "bordo"
+  }
+})
